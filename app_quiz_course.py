b4c96831bc275e3887945b1443c05c1ba4a248f3
thuthuy119 <81728914+thuthuy119@users.noreply.github.com>
2022-12-16 13:34:16 +0700

    Update app_quiz_course.py

--- app_quiz_course.py
+++ app_quiz_course.py
@@ -55,12 +55,12 @@ quiz = quiz.rename(columns = {'name':'title'})
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 tfid = TfidfVectorizer()
-vectorTfid =tfid.fit_transform(quiz['title']).toarray()
+vectorTfid =tfid.fit_transform(quiz['description']).toarray()
 similarityTfidVect = cosine_similarity(vectorTfid)
 
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(stop_words = None, lowercase = True)
-vector=cv.fit_transform(quiz['title']).toarray()
+vector=cv.fit_transform(quiz['description]).toarray()
 similarityCountVect = cosine_similarity(vector)
 
 # # from sklearn.feature_extraction.text import HashingVectorizer
